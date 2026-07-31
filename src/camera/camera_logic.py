6c09b44edfbe7229ed89e6dfb4eaea4b6bcc79e7
Hung260203 <hung2602vn@gmail.com>
2026-07-31 14:56:11 +1000

    Simple AE function

--- src/camera/camera_logic.py
+++ src/camera/camera_logic.py
@@ -1,38 +1,100 @@
-import sys
-import tty
-import termios
 from picamera2 import Picamera2, Preview
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Agg")
 
 class CameraLogic:
-	def __init__(self, config=None):
+	#Initialise camera
+	def __init__(self, manual=False, config=None):
 		self.picam2 = Picamera2()
-		self.config = config or  self.picam2.create_preview_configuration()
-		self.picam2.configure(self.config)
+		self.preview_config = self.picam2.create_preview_configuration()
+		self.still_config = self.picam2.create_still_configuration()
+		self.picam2.configure(self.preview_config)
 		self.preview_started = False
-		self.exposure = 4000
-		self.gain = 3.0
 
+	#Start camera
+	def start(self):
+		self.picam2.start()
+
+	#Stop and close camera
+	def close(self):
+		self.picam2.stop()
+		self.picam2.close()
+
+	#Display camera view
 	def start_preview(self, preview_type=Preview.DRM):
 		self.picam2.start_preview(preview_type)
 		self.preview_started = True
 		self.picam2.start()
 
+	#Stop camera preview
 	def stop_preview(self):
 		if self.preview_started:
 			self.picam2.stop_preview()
-		self.picam2.stop()
 
-	def close(self):
-		self.picam2.close()
-
+	#Return the camera parameters for calibrating
 	def supported_controls(self):
-		return self.picam2.camera_controls
+		print(self.picam2.camera_controls)
 
-	def set_controls(self, exposure, gain):
-		self.exposure = exposure
-		self.gain = gain
-		self.picam2.set_controls({"ExposureTime": self.exposure, "AnalogueGain": self.gain})
+	#Set the camera exposure time and gain values
+	def set_brightness(self, exposure, gain):
+		self.picam2.set_controls({"ExposureTime": exposure, "AnalogueGain": gain})
 
-	def get_controls(self):
-		return [self.exposure, self.gain]
+	#Get the camera metadata
+	def get_metadata(self):
+		return self.picam2.capture_metadata()
+
+	#Capture the current image
+	#Return an array with RGB channels
+	def capture(self):
+		return self.picam2.capture_array()[:,:,:3]
+
+	#Simple function to find the exposure time and gain to reach the target brightness
+	#Notes: This works but it converges to the target brightness very slowly, especially if it is overexposed.
+	#Next steps: - Try a calibration function, sweep through a range of exposure and gain to quickly get the correct setting
+	#            - Or find a math function that converges faster
+	#			 - Try a different method of auto exposure
+	def auto_exposure(self, current, target, plate_solving):
+		min_exposure, max_exposure = 1000, 1000000
+		min_gain, max_gain = 1.0, 16.0
+
+		frame = self.capture()
+		metadata = self.get_metadata()
+		exposure, gain = metadata["ExposureTime"], metadata["AnalogueGain"]
+
+		if plate_solving:
+			while current != target:
+				scale = target / current
+				if gain < max_gain:
+					gain *= scale
+				elif exposure < max_exposure:
+					exposure *= scale
+				gain = max_gain if gain > max_gain else gain
+				exposure = max_exposure if exposure > max_exposure else exposure
+
+				self.set_brightness(int(exposure), gain)
+				for _ in range(5):
+					self.capture()
+
+				print(f"Exposure: {exposure}, Gain: {gain}, Brightness: {current}")
+
+				frame = self.capture()
+				current = np.percentile(frame, 99)
+
+	def ae_plate_solving_mode(self):
+		self.picam2.set_controls({"AeEnable": False})
+
+		#This set controls is just for testing the mode
+		self.set_brightness(1000, 1.0)
+		for _ in range(5):
+			self.capture()
+
+		target_brightness = 253
+
+		while True:
+			frame = self.capture()
+			current_brightness = np.percentile(frame, 99)
+			if current_brightness != target_brightness:
+				self.exposure_product(current_brightness, target_brightness, plate_solving=True)
 
--- src/main.py
+++ src/main.py
@@ -1,44 +1,16 @@
 from camera import CameraLogic
-import sys
-import tty
-import termios
-
-def get_key():
-	fd = sys.stdin.fileno()
-	old = termios.tcgetattr(fd)
-	try:
-		tty.setraw(fd)
-		key = sys.stdin.read(1)
-	finally:
-		termios.tcsetattr(fd, termios.TCSADRAIN, old)
-	return key
+import numpy as np
+import time
 
 def main():
-	cam = CameraLogic()
-	exposure, gain = cam.get_controls()
-
+	cam = CameraLogic(manual=True)
 	cam.start_preview()
-
-	while True:
-		key = get_key()
-		if key == "-":
-			gain -= 0.1
-			print("Gain:", gain)
-		elif key == "=":
-			gain += 0.1
-			print("Gain:", gain)
-		elif key == "[":
-			exposure -= 100
-			print("Exposure:", exposure)
-		elif key == "]":
-			exposure += 100
-			print("Exposure:", exposure)
-		elif key == "q":
-			break
-		cam.set_controls(exposure, gain)
-
+	time.sleep(1)
+	cam.ae_plate_solving_mode()
+	input()
 	cam.stop_preview()
 	cam.close()
+
 
 if __name__ == "__main__":
 	main()
