2d2a8ea7a95097ba6edcf8cd1bd95a2525562330
Legolas4224 <thomas.s.kirkpatrick@hotmail.com>
2026-08-10 19:50:31 +1000

    improves histogram

--- histogram.py
+++ histogram.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import rawpy
+from datetime import datetime
 
 
 def load_tiff(path) :
@@ -49,9 +50,6 @@ def plot_histogram(img) :
     print("Max: ", np.max(img_array))
     print("Min: ", np.min(img_array))
 
-
-
-
     # 4. Calculate the 95th percentile
     #    This flattens the array and finds the value where 95% of pixels are below it
     brightness_threshold = np.percentile(img_array, 99)
@@ -69,18 +67,18 @@ def plot_histogram(img) :
         plt.hist(img_array.ravel(), bins=4000, range=(data_min, data_max), color="gray")
         plt.xlim(data_min, data_max)  # Forces the window to zoom in tight
 
-        
-    plt.hist(img_array.ravel(), bins=4000, range=(0, 65535), color="purple")
+    plt.hist(img_array.ravel(), bins=4000, range=(0, 65535), color="blue")
 
-    plt.title("Grayscale Histogram")
+    plt.title("Histogram")
     plt.xlabel("Pixel Intensity")
     plt.ylabel("Frequency")
-    #plt.yscale("log")
+    plt.yscale("log")
     #plt.xscale("log")
 
     # 3. Display the plot
     plt.tight_layout()
-    plt.show()
+    now = datetime.now()
+    plt.savefig(f"plots/{exp}s_{now.day}-{now.month}-{now.hour}:{now.minute}:{now.second}.png",)
 
     #plt.hist(image.ravel(), bins=1000)
     #plt.xlabel("Pixel value")
@@ -88,22 +86,23 @@ def plot_histogram(img) :
     #plt.title("Image Histogram")
     #plt.show()
 
-#plot_histogram("/home/thomas/Documents/Code/QuadStar/NewQuadstar/QuadStar_Auxillary_Computer/Kieran Test Images/QuadStar/20260807_183141_e-0.5_g-1.0_n-5.done/2026-08-07 08:31:44.825588+00:00.fits")
-
-# "/home/thomas/Documents/Code/QuadStar/Images/20260810_182334_e-0.5_g-1.0_n-5.solve/Ex499991_(0.5s)_Gain1.0_Temp14.0_1786350217.5650754.tiff"
-path = "/home/thomas/Documents/Code/QuadStar/Images/20260810_183233_e-30.0_g-1.0_n-5.solve/Ex6999327_(30.0s)_Gain1.0_Temp13.0_1786350760.9048686.tiff"
-
-
-exp = float(input("Exp time (s)"))
-if exp == 0.5 :
-    path = "/home/thomas/Documents/Code/QuadStar/Images/20260810_182334_e-0.5_g-1.0_n-5.solve/Ex499991_(0.5s)_Gain1.0_Temp14.0_1786350217.5650754.tiff"
-elif exp == 3 :
-    path = "/home/thomas/Documents/Code/QuadStar/Images/20260810_182428_e-3.0_g-1.0_n-5.solve/Ex2999948_(3.0s)_Gain1.0_Temp15.0_1786350271.9747188.tiff"
-elif exp == 30 :
+def main() :
     path = "/home/thomas/Documents/Code/QuadStar/Images/20260810_183233_e-30.0_g-1.0_n-5.solve/Ex6999327_(30.0s)_Gain1.0_Temp13.0_1786350760.9048686.tiff"
-elif exp == 1 :
-    path = "/home/thomas/Documents/Code/QuadStar/Images/ceiling_tiffs/Ex999982_(1.0s)_Gain1.0_Temp15.0_1785924314.6876094.tiff"
 
-print("loading image of exp time:", exp, "s")
-image = load_tiff(path)
-plot_histogram(image)
+
+    exp = float(input("Exp time (s): "))
+    if exp == 0.5 :
+        path = "/home/thomas/Documents/Code/QuadStar/Images/20260810_182334_e-0.5_g-1.0_n-5.solve/Ex499991_(0.5s)_Gain1.0_Temp14.0_1786350217.5650754.tiff"
+    elif exp == 3 :
+        path = "/home/thomas/Documents/Code/QuadStar/Images/20260810_182428_e-3.0_g-1.0_n-5.solve/Ex2999948_(3.0s)_Gain1.0_Temp15.0_1786350271.9747188.tiff"
+    elif exp == 30 :
+        path = "/home/thomas/Documents/Code/QuadStar/Images/20260810_183233_e-30.0_g-1.0_n-5.solve/Ex6999327_(30.0s)_Gain1.0_Temp13.0_1786350760.9048686.tiff"
+    elif exp == 1 :
+        path = "/home/thomas/Documents/Code/QuadStar/Images/ceiling_tiffs/Ex999982_(1.0s)_Gain1.0_Temp15.0_1785924314.6876094.tiff"
+
+    print("loading image of exp time:", exp, "s")
+    image = load_tiff(path)
+    plot_histogram(image)
+
+if __name__ == "__main__" :
+    main()
