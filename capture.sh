c39056389cab438bec1ff2e67dc59f3ccd648eeb
joosm4n <kotoole0703@gmail.com>
2026-08-17 11:45:52 +1000

    added folder make for heartbeats

--- capture.sh
+++ capture.sh
@@ -16,6 +16,7 @@ python src/main.py 10 1.0 1
 python src/auto_exposure.py
 
 # 'Heartbeat' image
+mkdir -p /home/pi/Heartbeats
 rpicam-still --camera 0 --zsl --shutter 500ms --gain 1.0 --awbgains 1,1 -o /home/pi/Heartbeats/Heartbeat_$(date '+%s').png
 
 echo "All exposures captured"
